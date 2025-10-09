9424fe5a86d1efac40114c684f8fc000841a8a39
Robin Castillo <robin.castillo@catolica.edu.sv>
2025-10-09 15:47:02 -0600

    cambios en add proveedor

--- database/migrations/2025_10_09_211656_add_proveedor_id_to_compras_table.php
+++ database/migrations/2025_10_09_211656_add_proveedor_id_to_compras_table.php
@@ -4,27 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::table('compras', function (Blueprint $table) {
-            $table->foreignId('proveedor_id')->nullable()->constrained()->onDelete('set null');
+            $table->unsignedBigInteger('proveedor_id')->nullable()->after('fecha'); // <- primero creas la columna
+            $table->foreign('proveedor_id')->references('id')->on('proveedores')->onDelete('set null');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-   
-public function down()
-{
-    Schema::table('compras', function (Blueprint $table) {
-        $table->dropForeign(['proveedor_id']);
-        $table->dropColumn('proveedor_id');
-    });
-}
+    public function down(): void
+    {
+        Schema::table('compras', function (Blueprint $table) {
+            $table->dropForeign(['proveedor_id']);
+            $table->dropColumn('proveedor_id');
+        });
+    }
 };
